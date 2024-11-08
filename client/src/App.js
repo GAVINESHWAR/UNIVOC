2669e0d1a30b83b0c38517f7c985371a87ca44f6
Univoc-Company <univoctech@gmail.com>
2024-11-08 13:05:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Benefits from "./components/benefit/Benefits";
+import BenefitDetail from "./components/benefitDetail/BenefitDetail";
 
 function App() {
+  
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Benefits />}></Route>
+          <Route path="/benefit/:id" element={<BenefitDetail />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
