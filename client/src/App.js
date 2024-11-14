5458df86d343066d792cbef09dcca7424f646f87
princi-sharma <princiinps@gmail.com>
2024-11-14 13:56:37 +0530

    updated App.js, Benefit.js and added Partners.js and Partners.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+// App.js
+
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import './index.css';
 
@@ -7,23 +9,26 @@ import BenefitDetail from "./components/benefitDetail/BenefitDetail.js";
 import Footer from "./components/footer/Footer.js";
 import HomeSection from "./components/HomeSection.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
+import Partners from "./components/partners/Partners";
 import Testimonials from "./components/testimonials/Testimonials.js";
 
 function AppContent() {
   const location = useLocation();
   const isAboutPage = location.pathname === "/about";
+  const isBenefitDetailPage = location.pathname.startsWith("/benefit/");
 
   return (
     <>
-      <Navbar />
-      {!isAboutPage && <HomeSection />}
+      {!isBenefitDetailPage && <Navbar />}
+      {!isAboutPage && !isBenefitDetailPage && <HomeSection />}
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Benefits />} />
         <Route path="/benefit/:id" element={<BenefitDetail />} />
       </Routes>
-      <Testimonials />
-      <Footer />
+      {!isBenefitDetailPage && <Partners />}
+      {!isBenefitDetailPage && <Testimonials />}
+      {!isBenefitDetailPage && <Footer />}
     </>
   );
 }
